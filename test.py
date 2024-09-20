f4d6eadee99d33f5fc048bc2e40c3e2dde0b0662
Gökhan Güzelkokar <gguzelkokar.mdbf15@iste.edu.tr>
2024-09-20 09:42:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 a = "test.n11.com"
 b = "n11.com"
-access_token_n11_test = "testmealone "
+access_token_n11_test = "testmealone"
+
+access_key = "43fdTHiSDSGSAgssdgfiStsagfadTesgdsTBcistadfasdfcaH"
+
